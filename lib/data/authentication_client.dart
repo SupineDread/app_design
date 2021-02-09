246b7ecb88bd26ba87527c12344fcf96c1010f8c
SupineDread <hazielfe@gmail.com>
2021-02-09 15:04:08 -0600

    To deploy

--- lib/data/authentication_client.dart
+++ lib/data/authentication_client.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter_api_rest/api/authentication_api.dart';
 import 'package:flutter_api_rest/models/authentication_response.dart';
@@ -8,9 +9,23 @@ class AuthenticationClient {
   final FlutterSecureStorage _secureStorage;
   final AuthenticationAPI _authenticationAPI;
 
+  Completer _completer;
+
   AuthenticationClient(this._secureStorage, this._authenticationAPI);
 
+  void _complete() {
+    if (_completer != null && !_completer.isCompleted) {
+      _completer.complete();
+    }
+  }
+
   Future<String> get accessToken async {
+    if (_completer != null) {
+      await _completer.future;
+    }
+
+    _completer = Completer();
+
     final data = await _secureStorage.read(key: 'SESSION');
     if (data != null) {
       final session = Session.fromJson(jsonDecode(data));
@@ -21,17 +36,21 @@ class AuthenticationClient {
       final diff = currentDate.difference(createdAt).inSeconds;
 
       if (expiresIn - diff >= 60) {
+        _complete();
         return session.token;
       }
 
       final response = await _authenticationAPI.refreshToken(session.token);
       if (response.data != null) {
         await this.saveSession(response.data);
+        _complete();
         return response.data.token;
       }
+      _complete();
 
       return null;
     }
+    _complete();
     return null;
   }
 
